3e4766069cae982e0731558ce3e0498a5f57bac3
Raish khaitu <71054834+dragneel101@users.noreply.github.com>
2021-12-15 00:28:18 -0500

    Add files via upload

---
+++ jetson_ready/yolo/yolo.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+import sys
+import cv2
+import depthai as dai
+import numpy as np
+import time
+import Jetson.GPIO as GPIO
+import screeninfo
+import matplotlib.pylab as plt
+import matplotlib
+import tkinter
+import numpy as np
+
+led_pin = 7
+buzzer_pin = 11
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setup(led_pin, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(buzzer_pin, GPIO.OUT, initial=GPIO.LOW)
+
+#mask the region of interest
+def region_of_interest(img, vertices):
+    mask = np.zeros_like(img)
+    match_mask_color = (255,)
+    cv2.fillPoly(mask, vertices, match_mask_color)
+    masked_image = cv2.bitwise_and(img, mask)
+    return masked_image
+
+def draw_the_lines(img, lines):
+    img = np.copy(img)
+    blank_image = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    print(lines)
+    if lines is None:
+        img = cv2.addWeighted(img, 0.8, blank_image, 1, 0.0)
+        return img
+    else:
+
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                cv2.line(blank_image, (x1, y1), (x2, y2), (0, 255, 0), thickness=10)
+        img = cv2.addWeighted(img, 0.8, blank_image, 1, 0.0)
+        return img
+
+def process(image):
+    print(image.shape)
+    height = image.shape[0]
+    width = image.shape[1]
+
+    #selecting the vertices for the region of interest
+    region_of_interest_vertices = [
+            (int(0.456 * width), int(0.65 * height)),  # Top-left corner
+            (0, height - 1),  # Bottom-left corner
+            (int(0.958 * width), height - 1),  # Bottom-right corner
+            (int(0.6183 * width), int(0.65 * height))  # Top-right corner
+           
+#        (0, height),
+ #       (width/2, height/1.9),
+  #      (width, height)
+    ]
+    gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+    gaus_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    canny_image = cv2.Canny(gaus_image, 100, 200)
+    cropped_image = region_of_interest(canny_image,
+                    np.array([region_of_interest_vertices], np.int32),)
+
+    lines = cv2.HoughLinesP(cropped_image,
+                            rho=6,
+                            theta=np.pi/60,
+                            threshold=170,
+                            lines=np.array([]),
+                            minLineLength=20,
+                            maxLineGap=20)
+    image_with_lines = draw_the_lines(image, lines)
+    return image_with_lines
+
+# get the size of the screen
+screen_id = 0
+is_color = False
+screen = screeninfo.get_monitors()[screen_id]
+width, height = screen.width, screen.height
+
+
+# Get argument first
+nnBlobPath = str((Path(__file__).parent / Path('../models/yolo-v4-tiny-tf_openvino_2021.4_6shave.blob')).resolve().absolute())
+
+if not Path(nnBlobPath).exists():
+    import sys
+    raise FileNotFoundError(f'Required file/s not found, please run "{sys.executable} install_requirements.py"')
+
+# Tiny yolo v3/4 label texts
+labelMap = [
+    "person",         "bicycle",    "car",           "motorbike",     "aeroplane",   "bus",           "train",
+    "truck",          "boat",       "traffic light", "fire hydrant",  "stop sign",   "parking meter", "bench",
+    "bird",           "cat",        "dog",           "horse",         "sheep",       "cow",           "elephant",
+    "bear",           "zebra",      "giraffe",       "backpack",      "umbrella",    "handbag",       "tie",
+    "suitcase",       "frisbee",    "skis",          "snowboard",     "sports ball", "kite",          "baseball bat",
+    "baseball glove", "skateboard", "surfboard",     "tennis racket", "bottle",      "wine glass",    "cup",
+    "fork",           "knife",      "spoon",         "bowl",          "banana",      "apple",         "sandwich",
+    "orange",         "broccoli",   "carrot",        "hot dog",       "pizza",       "donut",         "cake",
+    "chair",          "sofa",       "pottedplant",   "bed",           "diningtable", "toilet",        "tvmonitor",
+    "laptop",         "mouse",      "remote",        "keyboard",      "cell phone",  "microwave",     "oven",
+    "toaster",        "sink",       "refrigerator",  "book",          "clock",       "vase",          "scissors",
+    "teddy bear",     "hair drier", "toothbrush"
+]
+
+syncNN = True
+
+# Create pipeline
+pipeline = dai.Pipeline()
+
+# Define sources and outputs
+camRgb = pipeline.create(dai.node.ColorCamera)
+spatialDetectionNetwork = pipeline.create(dai.node.YoloSpatialDetectionNetwork)
+monoLeft = pipeline.create(dai.node.MonoCamera)
+monoRight = pipeline.create(dai.node.MonoCamera)
+stereo = pipeline.create(dai.node.StereoDepth)
+
+xoutRgb = pipeline.create(dai.node.XLinkOut)
+xoutNN = pipeline.create(dai.node.XLinkOut)
+xoutBoundingBoxDepthMapping = pipeline.create(dai.node.XLinkOut)
+xoutDepth = pipeline.create(dai.node.XLinkOut)
+
+xoutRgb.setStreamName("rgb")
+xoutNN.setStreamName("detections")
+xoutBoundingBoxDepthMapping.setStreamName("boundingBoxDepthMapping")
+xoutDepth.setStreamName("depth")
+
+# Properties
+camRgb.setPreviewSize(416, 416)
+camRgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_1080_P)
+camRgb.setInterleaved(False)
+camRgb.setColorOrder(dai.ColorCameraProperties.ColorOrder.BGR)
+
+monoLeft.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoLeft.setBoardSocket(dai.CameraBoardSocket.LEFT)
+monoRight.setResolution(dai.MonoCameraProperties.SensorResolution.THE_400_P)
+monoRight.setBoardSocket(dai.CameraBoardSocket.RIGHT)
+
+# setting node configs
+stereo.initialConfig.setConfidenceThreshold(255)
+
+spatialDetectionNetwork.setBlobPath(nnBlobPath)
+spatialDetectionNetwork.setConfidenceThreshold(0.6)
+spatialDetectionNetwork.input.setBlocking(False)
+spatialDetectionNetwork.setBoundingBoxScaleFactor(0.5)
+spatialDetectionNetwork.setDepthLowerThreshold(100)
+spatialDetectionNetwork.setDepthUpperThreshold(5000)
+
+# Yolo specific parameters
+spatialDetectionNetwork.setNumClasses(80)
+spatialDetectionNetwork.setCoordinateSize(4)
+spatialDetectionNetwork.setAnchors(np.array([10,14, 23,27, 37,58, 81,82, 135,169, 344,319]))
+spatialDetectionNetwork.setAnchorMasks({ "side26": np.array([1,2,3]), "side13": np.array([3,4,5]) })
+spatialDetectionNetwork.setIouThreshold(0.5)
+
+# Linking
+monoLeft.out.link(stereo.left)
+monoRight.out.link(stereo.right)
+
+camRgb.preview.link(spatialDetectionNetwork.input)
+if syncNN:
+    spatialDetectionNetwork.passthrough.link(xoutRgb.input)
+else:
+    camRgb.preview.link(xoutRgb.input)
+
+spatialDetectionNetwork.out.link(xoutNN.input)
+spatialDetectionNetwork.boundingBoxMapping.link(xoutBoundingBoxDepthMapping.input)
+
+stereo.depth.link(spatialDetectionNetwork.inputDepth)
+spatialDetectionNetwork.passthroughDepth.link(xoutDepth.input)
+
+# Connect to device and start pipeline
+with dai.Device(pipeline) as device:
+
+    # Output queues will be used to get the rgb frames and nn data from the outputs defined above
+    previewQueue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
+    detectionNNQueue = device.getOutputQueue(name="detections", maxSize=4, blocking=False)
+    xoutBoundingBoxDepthMappingQueue = device.getOutputQueue(name="boundingBoxDepthMapping", maxSize=4, blocking=False)
+    depthQueue = device.getOutputQueue(name="depth", maxSize=4, blocking=False)
+
+    startTime = time.monotonic()
+    counter = 0
+    fps = 0
+    color = (0, 255, 0)
+
+    while True:
+        
+        inPreview = previewQueue.get()
+        inDet = detectionNNQueue.get()
+        depth = depthQueue.get()
+
+        frame = inPreview.getCvFrame()
+        depthFrame = depth.getFrame()
+        depthFrameColor = cv2.normalize(depthFrame, None, 255, 0, cv2.NORM_INF, cv2.CV_8UC1)
+        depthFrameColor = cv2.equalizeHist(depthFrameColor)
+        depthFrameColor = cv2.applyColorMap(depthFrameColor, cv2.COLORMAP_HOT)
+
+        counter+=1
+        current_time = time.monotonic()
+        if (current_time - startTime) > 1 :
+            fps = counter / (current_time - startTime)
+            counter = 0
+            startTime = current_time
+
+        detections = inDet.detections
+        if len(detections) != 0:
+            boundingBoxMapping = xoutBoundingBoxDepthMappingQueue.get()
+            roiDatas = boundingBoxMapping.getConfigData()
+
+            for roiData in roiDatas:
+                roi = roiData.roi
+                roi = roi.denormalize(depthFrameColor.shape[1], depthFrameColor.shape[0])
+                topLeft = roi.topLeft()
+                bottomRight = roi.bottomRight()
+                xmin = int(topLeft.x)
+                ymin = int(topLeft.y)
+                xmax = int(bottomRight.x)
+                ymax = int(bottomRight.y)
+
+                cv2.rectangle(depthFrameColor, (xmin, ymin), (xmax, ymax), color, cv2.FONT_HERSHEY_SCRIPT_SIMPLEX)
+        else:
+            GPIO.output(led_pin, GPIO.LOW)
+            GPIO.output(buzzer_pin, GPIO.LOW)
+
+
+        # If the frame is available, draw bounding boxes on it and show the frame
+        height = frame.shape[0]
+        width  = frame.shape[1]
+        for detection in detections:
+            # Denormalize bounding box
+            x1 = int(detection.xmin * width)
+            x2 = int(detection.xmax * width)
+            y1 = int(detection.ymin * height)
+            y2 = int(detection.ymax * height)
+            try:
+                label = labelMap[detection.label]
+            except:
+                label = detection.label
+            cv2.putText(frame, str(label), (x1 + 10, y1 + 20), cv2.FONT_HERSHEY_TRIPLEX, 1, color)
+            cv2.putText(frame, "{:.2f}%".format((detection.confidence)*10), (x1 + 10, y1 + 35), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+            cv2.putText(frame, f"z:{int(detection.spatialCoordinates.z)}mm", (x1 + 10, y1 + 80), cv2.FONT_HERSHEY_TRIPLEX, 0.5, color)
+            if(int(detection.spatialCoordinates.z) < 1000 and int(detection.spatialCoordinates.z) != 0 ):
+                GPIO.output(led_pin, GPIO.HIGH)
+                GPIO.output(buzzer_pin, GPIO.HIGH) 
+                color = (0, 0, 255)
+                cv2.putText(frame,"warning!!", (x1 + 10, y1 + 60), cv2.FONT_HERSHEY_TRIPLEX, 1, color)
+
+            else:
+                GPIO.output(led_pin, GPIO.LOW)
+                GPIO.output(buzzer_pin, GPIO.LOW)
+                color = (0, 255, 0) 
+
+            cv2.rectangle(frame, (x1, y1), (x2, y2), color,2, cv2.FONT_HERSHEY_SIMPLEX)
+
+        cv2.putText(frame, "NN fps: {:.2f}".format(fps), (2, frame.shape[0] - 4), cv2.FONT_HERSHEY_TRIPLEX, 0.4, color)
+        width = int(frame.shape[1] * 100/ 100)
+        height = int(frame.shape[0])
+        dim = (width, height)
+        cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+        cv2.namedWindow("yolo", cv2.WND_PROP_FULLSCREEN)
+        cv2.setWindowProperty("yolo", cv2.WND_PROP_FULLSCREEN,cv2.WINDOW_FULLSCREEN)
+        frame = cv2.resize(frame, (480, 480))
+        frame = process(frame)
+        cv2.imshow("yolo",frame,)
+
+        if cv2.waitKey(1) == ord('q'):
+            GPIO.output(led_pin, GPIO.LOW)
+            GPIO.output(buzzer_pin, GPIO.LOW)
+            break
